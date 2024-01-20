39cf60d7ef7dda00f88d41672e620c4ae51cae2a
ᴍʀ ᴘᴇʀғᴇᴄᴛ x ᴛɪᴛᴀɴ (◕‿◕) <132195434+Mrperfectxd@users.noreply.github.com>
2024-01-20 18:07:49 +0530

    userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.one.join_chat("Musical_Beatsz")
+                await self.one.join_chat("TitanNetwrk")
             except:
                 pass
             assistants.append(1)
