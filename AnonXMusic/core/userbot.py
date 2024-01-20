433c8e2198c469c8f1ba96ae01f41ca346b9e9d1
ᴍʀ ᴘᴇʀғᴇᴄᴛ x ᴛɪᴛᴀɴ (◕‿◕) <132195434+Mrperfectxd@users.noreply.github.com>
2024-01-20 18:06:46 +0530

    userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.two.join_chat("FallenAssociation")
+                await self.two.join_chat("Musical_Beatsz")
+                await self.two.join_chat("TitanNetwrk")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
-                await self.three.join_chat("FallenAssociation")
+                await self.three.join_chat("Musical_Beatsz")
+                await self.three.join_chat("TitanNetwrk")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
-                await self.four.join_chat("FallenAssociation")
+                await self.four.join_chat("Musical_Beatsz")
+                await self.four.join_chat("TitanNetwrk")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("DevilsHeavenMF")
-                await self.five.join_chat("FallenAssociation")
+                await self.five.join_chat("Musical_Beatsz")
+                await self.five.join_chat("TitanNetwrk")
             except:
                 pass
             assistants.append(5)
