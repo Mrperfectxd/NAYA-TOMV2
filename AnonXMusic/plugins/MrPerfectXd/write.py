69a351cd48410eb025ed103b0ce6bc280e4b2727
ᴍʀ ᴘᴇʀғᴇᴄᴛ x ᴛɪᴛᴀɴ (◕‿◕) <132195434+Mrperfectxd@users.noreply.github.com>
2024-01-21 01:16:52 +0530

    write.py

--- AnonXMusic/plugins/MrPerfectXd/write.py
+++ AnonXMusic/plugins/MrPerfectXd/write.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from config import  BOT_USERNAME
-from AloneXMusic import app as app
+from AnonXMusic import app as app
 import requests
 
 @app.on_message(filters.command("write"))
